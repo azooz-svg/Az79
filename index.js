264367a11ab4db1b75a2c9a6757d466016bc2b21
azooz-svg <azoozshehada@gmail.com>
2026-05-29 02:02:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Az79 Bot is online! Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong! البوت شغال!');
+  }
+
+  if (message.content === '!help') {
+    message.reply(`
+**Az79 Bot - قائمة الأوامر:**
+\`!ping\` - تحقق من البوت
+\`!help\` - قائمة الأوامر
+\`!info\` - معلومات السيرفر
+    `);
+  }
+
+  if (message.content === '!info') {
+    message.reply(`**السيرفر:** ${message.guild.name}\n**الأعضاء:** ${message.guild.memberCount}`);
+  }
+});
+
+client.login(process.env.TOKEN);
